198d5e4d4dc5e59220dccef3fcb7d1699d8f94b7
Björn Nyman <bjorn.nyman@webflex.se>
2025-09-16 14:57:37 +0200

    Create Instrument Table

---
+++ Create Instrument Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Instruments (
+    InstrumentId INT IDENTITY(1,1) NOT NULL,   -- Auto-increment
+    Name NVARCHAR(100) NOT NULL,               -- Instrumentnamn
+    Family NVARCHAR(100) NULL,                 -- T.ex. Brass, Träblås, Slagverk
+
+    CONSTRAINT PK_Instruments PRIMARY KEY (InstrumentId),
+    CONSTRAINT UQ_Instruments_Name UNIQUE (Name)  -- Unikt namn per instrument
+);
