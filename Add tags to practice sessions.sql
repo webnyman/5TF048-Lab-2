a7d72e006917960cbae43bbe079430c7ec7b9c3d
Björn Nyman <bjorn.nyman@webflex.se>
2025-09-16 16:32:38 +0200

    Get all sessions with tags

---
+++ Add tags to practice sessions.sql
@@ -0,0 +1,12 @@
+﻿SELECT TOP 3 SessionId FROM dbo.PracticeSessions ORDER BY SessionId;
+SELECT TagId, Name FROM dbo.Tag;
+
+-- Lägg till taggar till övningssessioner
+INSERT INTO dbo.PracticeSessionTag (SessionId, TagId) VALUES
+(1, 1), -- Teknik
+(1, 2), -- Etyder
+(1, 3), -- Skalor
+(2, 1), -- Konsertförberedelse
+(2, 2), -- Teknik
+(3, 5), -- Uppvärmning
+(4, 1); -- Teknik
---
+++ Get all sessions with tags and instruments.sql
@@ -0,0 +1,8 @@
+﻿-- Description: Query to retrieve practice sessions with their associated instruments and tags
+
+SELECT ps.SessionId, ps.PracticeDate, i.Name AS Instrument, t.Name AS Tag
+FROM dbo.PracticeSessions ps
+JOIN dbo.Instruments i ON i.InstrumentId = ps.InstrumentId
+LEFT JOIN dbo.PracticeSessionTag pst ON pst.SessionId = ps.SessionId
+LEFT JOIN dbo.Tag t ON t.TagId = pst.TagId
+ORDER BY ps.SessionId, t.Name;
