22384175eebb52338b22c2046d42773464a1d6aa
Björn Nyman <bjorn.nyman@webflex.se>
2025-09-16 15:27:27 +0200

    Add tags

---
+++ Add PracticeSessionTags.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO dbo.Tag (Name) VALUES
+(N'Teknik'), (N'Etyder'), (N'Skalor'), (N'Konsertförberedelse'), (N'Uppvärmning');
---
+++ Create PracticeSessionTag Table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE dbo.PracticeSessionTag (
+    SessionId INT NOT NULL,
+    TagId     INT NOT NULL,
+
+    CONSTRAINT PK_PracticeSessionTag PRIMARY KEY (SessionId, TagId),
+
+    CONSTRAINT FK_PST_PracticeSessions
+        FOREIGN KEY (SessionId) REFERENCES dbo.PracticeSessions(SessionId)
+        ON DELETE CASCADE,
+
+    CONSTRAINT FK_PST_Tag
+        FOREIGN KEY (TagId) REFERENCES dbo.Tag(TagId)
+        ON DELETE CASCADE
+);
