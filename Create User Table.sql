1eebf00fb17ede7dad8fc4f6dec48a5b49cffdde
Björn Nyman <bjorn.nyman@webflex.se>
2025-09-16 14:37:19 +0200

    Add user table

---
+++ Create User Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE Users (
+    UserId UNIQUEIDENTIFIER NOT NULL 
+        DEFAULT NEWID(),           -- Genererar GUID automatiskt
+    Email NVARCHAR(256) NOT NULL, -- Unik e-post
+    DisplayName NVARCHAR(128) NOT NULL,
+    CreatedAt DATETIME2 NOT NULL 
+        DEFAULT SYSUTCDATETIME(), -- Timestamp
+
+    CONSTRAINT PK_Users PRIMARY KEY (UserId),
+    CONSTRAINT UQ_Users_Email UNIQUE (Email)
+);
