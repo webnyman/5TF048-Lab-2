73f39af986455960f6cab04a8255ba581e3b7cec
Björn Nyman <bjorn.nyman@webflex.se>
2025-09-17 09:55:34 +0200

    Delete one practice session by id

---
+++ Delete one practice session by id.sql
@@ -0,0 +1,6 @@
+﻿--- Delete one practice session by its ID
+DELETE FROM dbo.PracticeSessions
+WHERE SessionId = 1;
+-- Verify deletion
+SELECT * FROM dbo.PracticeSessions WHERE SessionId = 1;
+-- End of script
