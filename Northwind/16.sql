03760ed8c8873474d94e6c4aa8a53b134e95c021
Björn Nyman <bjorn.nyman@webflex.se>
2025-09-19 09:52:16 +0200

    Add transaction

--- Northwind/16.sql
+++ Northwind/16.sql
@@ -1,5 +1,4 @@
 ﻿--- Uppgift 16
--- Totalsumma för alla ordrar 1996-12-12
 SELECT SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)) AS [Total ordersumma]
 FROM Orders o
 JOIN [Order Details] od ON od.OrderID = o.OrderID
---
+++ T Uppdatera två värden.sql
@@ -0,0 +1,27 @@
+﻿DECLARE @SessionId INT = 2;  -- byt till en existerande session
+
+BEGIN TRY
+    BEGIN TRAN;
+
+    -- 1) Uppdatera antalet minuter
+    UPDATE dbo.PracticeSessions
+    SET Minutes = Minutes + 15
+    WHERE SessionId = @SessionId;
+
+    -- 2) Försök sätta en ogiltig intensitet (bryter CHECK: måste vara 1–5)
+    UPDATE dbo.PracticeSessions
+    SET Intensity = 6
+    WHERE SessionId = @SessionId;
+
+    COMMIT; -- når vi bara om båda lyckas
+    PRINT 'Transaktion COMMIT: båda uppdateringar sparade';
+END TRY
+BEGIN CATCH
+    ROLLBACK; -- återställ om något fel inträffar
+    PRINT 'Transaktion ROLLBACK: inga uppdateringar sparade';
+END CATCH;
+
+-- Kontrollera att värdena inte ändrats om rollback triggades
+SELECT SessionId, Minutes, Intensity
+FROM dbo.PracticeSessions
+WHERE SessionId = @SessionId;
