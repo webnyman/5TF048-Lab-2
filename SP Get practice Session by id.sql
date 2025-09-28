79effe2bb08bd14c3591a0c79bdb245f0f667a9f
Björn Nyman <bjorn.nyman@webflex.se>
2025-09-28 12:14:25 +0200

    Add get practice session by id

---
+++ SP Get practice Session by id.sql
@@ -0,0 +1,9 @@
+﻿CREATE OR ALTER PROCEDURE dbo.usp_GetPracticeSessionById
+  @SessionId INT
+AS
+BEGIN
+  SET NOCOUNT ON;
+  SELECT SessionId, InstrumentId, PracticeDate, Minutes, Intensity, Focus, Comment
+  FROM dbo.PracticeSessions
+  WHERE SessionId = @SessionId;
+END
