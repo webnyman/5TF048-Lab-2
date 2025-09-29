55ed89e6c127c629c2ab5ba565bc54db5f2c329f
Björn Nyman <bjorn.nyman@webflex.se>
2025-09-29 12:23:21 +0200

    Fix isuues with sp create, update and delete on instrument

--- SP CRUD Instrument.sql
+++ SP CRUD Instrument.sql
@@ -34,20 +34,61 @@ GO
 
 CREATE OR ALTER PROCEDURE dbo.usp_Instruments_Update
   @InstrumentId INT,
-  @Name NVARCHAR(100),
-  @Family NVARCHAR(50)
+  @Name         NVARCHAR(100),
+  @Family       NVARCHAR(50)
 AS
 BEGIN
-  SET NOCOUNT ON;
-  UPDATE dbo.Instruments SET Name=@Name, Family=@Family WHERE InstrumentId=@InstrumentId;
+  SET NOCOUNT ON; SET XACT_ABORT ON;
+
+  BEGIN TRY
+    BEGIN TRAN;
+
+    UPDATE dbo.Instruments
+    SET Name = @Name, Family = @Family
+    WHERE InstrumentId = @InstrumentId;
+
+    DECLARE @rc INT = @@ROWCOUNT;
+
+    IF @rc = 0
+      THROW 50020, 'Instrument not found.', 1;
+
+    COMMIT;
+
+    SELECT @rc AS RowsAffected;
+  END TRY
+  BEGIN CATCH
+    IF XACT_STATE() <> 0 ROLLBACK;
+    THROW;
+  END CATCH
 END
 GO
+
 
 CREATE OR ALTER PROCEDURE dbo.usp_Instruments_Delete
   @InstrumentId INT
 AS
 BEGIN
-  SET NOCOUNT ON;
-  DELETE FROM dbo.Instruments WHERE InstrumentId=@InstrumentId;
+  SET NOCOUNT ON; SET XACT_ABORT ON;
+
+  BEGIN TRY
+    BEGIN TRAN;
+
+    DELETE FROM dbo.Instruments
+    WHERE InstrumentId = @InstrumentId;
+
+    DECLARE @rc INT = @@ROWCOUNT;
+
+    IF @rc = 0
+      THROW 50021, 'Instrument not found.', 1;
+
+    COMMIT;
+
+    SELECT @rc AS RowsAffected;
+  END TRY
+  BEGIN CATCH
+    IF XACT_STATE() <> 0 ROLLBACK;
+    THROW;
+  END CATCH
 END
 GO
+
