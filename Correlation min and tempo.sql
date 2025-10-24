b8a8bdfbd2c4cb544c427809d2695e932b07ab3a
Björn Nyman <bjorn.nyman@webflex.se>
2025-10-24 11:20:19 +0200

    Fix issues in SP Summary

--- Correlation min and tempo.sql
+++ Correlation min and tempo.sql
@@ -1,1 +1,35 @@
-﻿
+﻿DECLARE @UserEmail NVARCHAR(256) = N'info@trumpet.se';
+
+;WITH U AS (
+  SELECT TOP 1 CAST(Id AS UNIQUEIDENTIFIER) AS UserId
+  FROM dbo.AspNetUsers
+  WHERE NormalizedEmail = UPPER(@UserEmail)
+),
+B AS (
+  SELECT
+      CAST(ps.Minutes AS FLOAT)                 AS x,   -- Minutes
+      CAST(ps.TempoEnd - ps.TempoStart AS FLOAT) AS y   -- DeltaTempo
+  FROM dbo.PracticeSessions ps
+  CROSS JOIN U
+  JOIN dbo.Instruments i ON i.InstrumentId = ps.InstrumentId
+  WHERE ps.UserId = U.UserId
+    AND i.Name = N'Trumpet'
+    AND ps.TempoStart IS NOT NULL
+    AND ps.TempoEnd   IS NOT NULL
+),
+S AS (
+  SELECT
+      COUNT(*)               AS n,
+      SUM(x)                 AS sumx,
+      SUM(y)                 AS sumy,
+      SUM(x*y)               AS sumxy,
+      SUM(x*x)               AS sumx2,
+      SUM(y*y)               AS sumy2
+  FROM B
+)
+SELECT
+    CASE 
+      WHEN n < 2 THEN NULL
+      ELSE (n*sumxy - sumx*sumy) / NULLIF(SQRT((n*sumx2 - sumx*sumx)*(n*sumy2 - sumy*sumy)),0)
+    END AS PearsonR_Minutes_vs_DeltaTempo
+FROM S;
